8a3e4de3d1cfb3726561ff8870e5c96d673bfe90
JenniferHuayatla <jennifer.huayatla@gmail.com>
2023-07-18 12:56:11 -0600

    Se agrega la interfaz de Login para los administratvos

---
+++ app/src/main/java/com/example/puntoventacafeteria/loginAdministrativo.java
@@ -0,0 +1,14 @@
+package com.example.puntoventacafeteria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginAdministrativo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_administrativo);
+    }
+}
